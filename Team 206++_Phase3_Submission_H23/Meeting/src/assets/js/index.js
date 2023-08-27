1dcf361a1a59c048f60aad7c0335ccbd35f7e604
DharmaBagadia004 <125524527+DharmaBagadia004@users.noreply.github.com>
2023-08-27 09:54:22 +0530

    Add files via upload

---
+++ Team 206++_Phase3_Submission_H23/Meeting/src/assets/js/index.js
@@ -0,0 +1,9 @@
+setTimeout(() => {alert("Quiz Time")
+document.getElementById('quiz').style.display='block'
+document.getElementById('vdo').style.display='none'
+}, 10000);
+
+const handle=()=>{
+    document.getElementById('quiz').style.display='none'
+document.getElementById('vdo').style.display='block'
+}
